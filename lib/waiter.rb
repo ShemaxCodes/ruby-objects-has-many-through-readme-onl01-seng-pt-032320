5fc7794b1d3d40541a961b77d5efe55d9c0dd305
Jashema Panapa <jcpanapa@gmail.com>
2020-04-22 05:35:26 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -22,9 +22,10 @@ def meals
   end 
 end 
 
-def best_tipper 
-  best_tipped_meal = meals.max do |meal
-end 
-
+def best_tipper
+  best_tipped_meal = meals.max do |meal_a, meal_b|
+    meal_a.tip <=> meal_b.tip
+  end
+ 
 
 end
